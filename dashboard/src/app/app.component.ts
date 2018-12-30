31fa2b4bcbff31814bb7898ca0d10fb344d05ff5
rozsabence <robence@gmail.com>
2018-12-30 12:12:25 +0100

    add socket connection poc

--- dashboard/src/app/app.component.ts
+++ dashboard/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from './chat.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'carbonara-dashboard';
+export class AppComponent implements OnInit{
+  title = 'app';
+  MSG = 'Test Message';
+
+  constructor(private chat: ChatService) { }
+
+  ngOnInit() {
+    this.chat.messages.subscribe(msg => {
+      console.log(msg);
+      this.sendMessage();
+    });
+  }
+
+  sendMessage() {
+    this.chat.sendMsg(this.MSG);
+  }
+
 }
